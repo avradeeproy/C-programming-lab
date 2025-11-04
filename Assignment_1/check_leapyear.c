952b4cdf475eb16a0fb05ea77c5f551100b984d6
Avradeep Roy <avradeeproy725@gmail.com>
2025-11-04 07:18:42 +0530

    Create check_leapyear.c

---
+++ Assignment_1/check_leapyear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // getting the year as input
+    printf("Year: ");
+    int year;
+    scanf("%d", &year);
+    
+    // check for leap year
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("Leap year\n");
+    else
+        printf("Not a leap year\n");
+    
+    
+    return 0;
+}
