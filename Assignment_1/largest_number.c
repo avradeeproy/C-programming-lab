47cc95dfb7d06c3a4ed944f7e4301ccca99d6415
Avradeep Roy <avradeeproy725@gmail.com>
2025-11-04 07:21:47 +0530

    Create largest_number.c

---
+++ Assignment_1/largest_number.c
@@ -0,0 +1,21 @@
+int main(void)
+{
+	// getting the user input
+	int x, y, z;
+	printf("x: ");
+	scanf("%d", &x);
+	printf("y: ");
+	scanf("%d", &y);
+	printf("z: ");
+	scanf("%d", &z);
+
+	// doing the comparison
+	if (x > y && x > z)
+		printf("%d is largest\n", x);
+	else if (y > x && y > z)
+		printf("%d is largest\n", y);
+	else if (z > x && z > y)
+		printf("%d is largest\n", z);
+	else
+		printf("All are equal\n");
+}
