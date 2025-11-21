a4cc76391604c188b3065534c534d4bc707b69aa
Avradeep Roy <avradeeproy725@gmail.com>
2025-11-21 22:07:02 +0530

    Create intro.c

---
+++ Lab_1/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is Avradeep Roy\n");
+    printf("My branch iss Mechanical Engineering\n");
+    printf("My roll no is MEB25064\n ");
+    return 0;
+}
