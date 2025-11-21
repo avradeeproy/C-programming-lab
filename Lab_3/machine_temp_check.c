8132da4801c896b671e71ce69c8bd9f0e0f5bf7b
Avradeep Roy <avradeeproy725@gmail.com>
2025-11-21 21:52:16 +0530

    Create machine_temp_check.c

---
+++ Lab_3/machine_temp_check.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+    float temp;
+    printf("Enter the temperature of your machine in Celsius:\n ");
+    scanf("%f", &temp);
+    if (temp<=80 && temp>60)
+        printf("Your machine is within safe limits.\n");
+    else
+        printf("Warning! Your machine temperature is out of safe range \n");
+    return 0;
+}
